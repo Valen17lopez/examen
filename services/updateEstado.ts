4fdbda17dad16aa04cbce02db9bdcb51a625ca8e
Valen <valenag.0703@gmail.com>
2024-05-14 09:56:44 -0500

    evaluacion

--- services/updateEstado.ts
+++ services/updateEstado.ts
@@ -1,21 +1,20 @@
 import UserRepository from '../repositories/UserRepository';
 import UpdateEstado from '../Dto/UpdateEstadoDto';
 
+
 class EstadoServices {
     static async UpdateEstado(id_pedi:number, estado_pedi:string) {
         try {
             const estdo: UpdateEstado ={
                 id_pedi,
                 estado_pedi
-            }
-            const result = await UserRepository.updateEstado(estado_pedi)
-            return result
+            };
+            const result = await UserRepository.updateEstado(estdo); 
+            return result;
         } catch (error) {
             throw error;
         }
     }
 }
-
-
 
 export default EstadoServices;
